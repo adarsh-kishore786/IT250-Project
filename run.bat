aab7ccf3153a00ee1a50b10a69e68dd04fd1601b
famousfive1 <tejaswihegde1@gmail.com>
2022-04-15 19:47:35 +0530

    update runner

--- run.bat
+++ run.bat
@@ -1,10 +1,17 @@
 @echo off
+
+IF %1.==. GOTO No1
+
 echo FLEX START
 flex -o tokenizer.yy.c tokenizer.l
+
 echo BISON START
 bison -o parser.tab.c -d parser.y
+
 echo COMPILING
 gcc -o compiler.exe tokenizer.yy.c parser.tab.c
+
+:No1
 echo RUNNING
 echo.
 .\compiler input.txt
